cf5de4a4004d3f0f645e220ffcf2eb8932363705
Brian D. Frost <brian.frost@sonicdrivein.com>
2019-05-28 13:41:52 -0500

    added firewall

--- main.tf
+++ main.tf
@@ -22,33 +22,36 @@ resource "random_id" "instance_id" {
 }
 
 // A single Google Cloud Engine instance
-resource "google_compute_instance" "default" {
- name         = "mtv-vm-${random_id.instance_id.hex}"
- machine_type = "f1-micro"
- zone         = "us-central1-a"
-
- boot_disk {
+resource "google_compute_instance" "webserver" {
+  name         = "mtv-vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  boot_disk {
    initialize_params {
      image = "debian-cloud/debian-9"
    }
- }
+  }
+  metadata = {
+    sshKeys = "bfrost:${file("~/.ssh/id_rsa.pub")}"
+  }
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
 
-metadata {
-   sshKeys = "bfrost:${file("~/.ssh/id_rsa.pub")}"
-}
-
-// Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-
- network_interface {
-   network = "default"
-   access_config {
-     // Include this section to give the VM an external ip address
+  network_interface {
+    network = "default"
+    access_config {
+      // Include this section to give the VM an external ip address
    }
  }
 }
+resource "google_compute_firewall" "webfirewall" {
+ name    = "flask-app-firewall"
+ network = "default"
 
-// A variable for extracting the external ip of the instance
+ allow {
+   protocol = "tcp"
+   ports    = ["5000"]
+ }
+}
 output "ip" {
- value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+ value = "${google_compute_instance.webserver.network_interface.0.access_config.0.nat_ip}"
 }
